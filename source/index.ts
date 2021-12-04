2f95814f7b52b71de43ca4c01b01e2330f2f91d3
mvllow <mvllow@icloud.com>
2021-12-04 13:13:25 -0600

    fix: check for invalid hex characters

--- source/index.ts
+++ source/index.ts
@@ -1,45 +1,58 @@
-type Color = string | { [key: string]: string }
-type Opacity = number
+const hexCharacters = 'a-f\\d'
+const match3or4Hex = `#?[${hexCharacters}]{3}[${hexCharacters}]?`
+const match6or8Hex = `#?[${hexCharacters}]{6}([${hexCharacters}]{2})?`
+const nonHexChars = new RegExp(`[^#${hexCharacters}]`, 'gi')
+const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i')
 
-function convertDecimalToHex(decimal: number) {
-	const hex = Math.round(decimal * 255).toString(16)
+function alphaToHex(alpha: number) {
+	const hex = Math.round(alpha * 255).toString(16)
 
 	if (hex.length === 1) return `0${hex}`
 	if (hex.length !== 2) return 'ff'
 	return hex
 }
 
-function normalizeHex(hex: string) {
-	let workingHex = hex.replace('#', '')
-
-	if (workingHex.length !== 3 && workingHex.length !== 6) {
-		throw new Error(`Invalid hex code: ${hex}`)
-	}
-
-	if (workingHex.length === 3) {
-		workingHex = workingHex.repeat(2)
-	}
-
-	return `#${workingHex}`
-}
-
-function colorish(color: Color, opacity: Opacity) {
+function colorish(color: string | Record<string, string>, alpha: number) {
 	if (typeof color === 'object') {
-		const colors: typeof color = {}
+		const colors = {}
 
 		for (const key in color) {
-			if (typeof color[key] === 'object') {
-				throw new Error('Nested color objects are not supported')
-			}
+			if (color[key] !== 'undefined') {
+				if (typeof color[key] === 'object') {
+					throw new TypeError('Nested objects are not supported')
+				}
 
-			Object.assign(colors, { [key]: colorish(color[key], opacity) })
+				Object.assign(colors, {[key]: colorish(color[key] ?? '', alpha)})
+			}
 		}
 
 		return colors
 	}
 
-	return `${normalizeHex(color)}${convertDecimalToHex(opacity)}`
+	if (
+		typeof color !== 'string' ||
+		nonHexChars.test(color) ||
+		!validHexSize.test(color)
+	) {
+		throw new TypeError('Expected a valid hex string')
+	}
+
+	const hexAlpha = alphaToHex(alpha)
+
+	let hex = color.replace(/^#/, '')
+
+	if (hex.length === 3) {
+		hex = `${hex.charAt(0)}${hex.charAt(0)}${hex.charAt(1)}${hex.charAt(
+			1
+		)}${hex.charAt(2)}${hex.charAt(2)}`
+	}
+
+	if (hex.length !== 6) {
+		throw new TypeError('Expected valid hex length')
+	}
+
+	return `#${hex}${hexAlpha}`
 }
 
+export {colorish}
 export default colorish
-export { colorish }
